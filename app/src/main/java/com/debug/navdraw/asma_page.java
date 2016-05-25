2a796359f46198296fa3341c91ab42bc67048d55
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-05-26 01:36:15 +0700

    list view di asma beres
    ceritanya konyol deh wkak
    tinggal diisi aja tulisan dkk wka

--- app/src/main/java/com/debug/navdraw/asma_page.java
+++ app/src/main/java/com/debug/navdraw/asma_page.java
@@ -1,41 +1,34 @@
 package com.debug.navdraw;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
+public class asma_page extends AppCompatActivity {
+    ListView list;
+    String[] web = {
+            "Google Plus",
+            "Twitter",
+            "Windows"
 
-public class asma_page extends Activity {
+    } ;
+    Integer[] imageId = {
+            R.drawable.icon_angka_satu,
+            R.drawable.icon_angka_dua,
+            R.drawable.icon_angka_tiga
 
-    ListView lv;
-    Context context;
-
-    ArrayList prgmName;
-    public static int [] angkaImage={R.drawable.icon_angka_satu,R.drawable.icon_angka_dua,R.drawable.icon_angka_tiga};
-    public static String[] asmaText={"c++","JAVA","Jsp"};
-    TextView textView = (TextView) findViewById(R.id.tv_Asma);
-
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asma_page);
 
-        context=this;
 
-        lv=(ListView) findViewById(R.id.listAsma);
-        lv.setAdapter(new ListAsmaAdapter(this,asmaText,angkaImage));
-    }
+        ListAsmaAdapter adapter = new ListAsmaAdapter(asma_page.this, web, imageId);
+        list=(ListView)findViewById(R.id.listAsma);
+        list.setAdapter(adapter);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
 }
