2583ad5fcbd1ee28cee50707fb2afe10fbe154de
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-05-29 22:12:42 +0700

    masukin search di tiap page

--- app/src/main/java/com/debug/navdraw/tombol_darurat_page.java
+++ app/src/main/java/com/debug/navdraw/tombol_darurat_page.java
@@ -1,10 +1,12 @@
 package com.debug.navdraw;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class tombol_darurat_page extends AppCompatActivity {
@@ -32,7 +34,29 @@ public class tombol_darurat_page extends AppCompatActivity {
             case android.R.id.home:
                 finish();
                 return true;
+
         }
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.search_setting) {
+            Intent intent = new Intent(tombol_darurat_page.this, search_function.class);
+            startActivity(intent);
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
 }
